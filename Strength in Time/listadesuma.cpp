8e86fa8e200635ab121dcec9a1acdc1956e6d243
Daniel <darodriguezto@unal.edu.co>
2023-06-11 23:56:10 -0500

    Se corrige la lista de suma con los archivos que realmente existen

--- Strength in Time/listadesuma.cpp
+++ Strength in Time/listadesuma.cpp
@@ -3,45 +3,50 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <boost/filesystem.hpp>
 
-using namespace std;
+namespace fs = boost::filesystem;
 
-int main(int argc,char **argv) {
-  std::string year=argv[1];
-  string folderpath = year + "/";
-  string filename = folderpath +"archivo.txt";
-  ifstream infile(filename);
-  string line;
+int main(int argc, char **argv) {
+  std::string year = argv[1];
+  std::string folderpath = year + "/";
+  std::string filename = folderpath + "archivo.txt";
+  std::ifstream infile(filename);
+  std::string line;
 
-  while (getline(infile, line)) {
+  while (std::getline(infile, line)) {
     double total = 0.0;
-    string datafile = folderpath+ line;
-    ifstream datastream(datafile);
-    string dataline;
+    std::string datafile = folderpath + line;
+
+    if (!fs::exists(datafile)) {
+      std::cout << "El archivo no existe: " << datafile << std::endl;
+      continue; // Ignora el archivo y pasa al siguiente
+    }
+
+    std::ifstream datastream(datafile);
+    std::string dataline;
     int line_count = 0;
 
-    while (getline(datastream, dataline)) {
+    while (std::getline(datastream, dataline)) {
       line_count++;
       if (line_count < 8) {
-	continue; // Salta las primeras siete líneas                                                                                                                                                                                  
+        continue; // Salta las primeras siete líneas
       }
-      // Separa la línea en campos usando el delimitador de espacio en blanco                                                                                                                                                           
-      istringstream ss(dataline);
-      string field;
+      std::istringstream ss(dataline);
+      std::string field;
       for (int i = 0; i < 3; i++) {
-	// Descarta los primeros dos campos                                                                                                                                                                                           
-	ss >> field;
+        ss >> field; // Descarta los primeros dos campos
       }
-      // Si estamos en la octava línea o más, lee el valor de la tercera columna y lo agrega al total                                                                                                                                   
       if (line_count >= 8) {
-	double value;
-	ss >> value;
-	total += value;
+        double value;
+        ss >> value;
+        total += value;
       }
     }
-    cout << datafile << "\t" << total << endl;
+    std::cout << datafile << "\t" << total << std::endl;
     datastream.close();
   }
   infile.close();
   return 0;
 }
+
