6cbd27fcf5339bbcf31287c90e8307042ac8e3ee
daniel <darodriguezto@unal.edu.co>
2023-08-03 02:07:15 -0500

    Se muestra la fecha en vez del nombre del archivo txt

--- Strength in Time/2011/NOA_NearSide_Data/new_ARList.py
+++ Strength in Time/2011/NOA_NearSide_Data/new_ARList.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def is_numero(cadena):
     try:
@@ -6,6 +7,15 @@ def is_numero(cadena):
         return True
     except ValueError:
         return False
+
+def obtener_fecha_desde_nombre(nombre_archivo):
+    # Buscar el patrón de fecha en el nombre del archivo
+    patron_fecha = re.search(r'(\d{4})(\d{2})(\d{2})', nombre_archivo)
+    if patron_fecha:
+        anio, mes, dia = patron_fecha.groups()
+        return f"{anio}-{mes}-{dia}"
+    else:
+        return "Fecha no encontrada"
 
 def leer_datos_archivo(nombre_archivo):
     with open(nombre_archivo, 'r') as archivo:
@@ -18,21 +28,22 @@ def encontrar_datos_nuevos():
 
     datos_archivos = []
     for archivo in archivos_txt:
-        print(f"Leyendo archivo: {archivo}")
+        #print(f"Leyendo archivo: {archivo}")
+        fecha = obtener_fecha_desde_nombre(archivo)
         datos_archivo = leer_datos_archivo(archivo)
-        datos_archivos.append(datos_archivo)
+        datos_archivos.append((fecha, datos_archivo))
 
     datos_primer_vez = set()
-    for datos_archivo in datos_archivos:
+    for fecha, datos_archivo in datos_archivos:
         datos_nuevos = datos_archivo - datos_primer_vez
         datos_primer_vez.update(datos_archivo)
 
         if datos_nuevos:
-            print(f"{archivos_txt[datos_archivos.index(datos_archivo)]}")
+            print(f"{fecha}")
             print(", ".join(str(dato) for dato in datos_nuevos))
             print()
         else:
-            print(f"{archivos_txt[datos_archivos.index(datos_archivo)]}")
+            print(f"{fecha}")
             print()
 
 encontrar_datos_nuevos()
