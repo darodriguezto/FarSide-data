63925dcd44efe86c3c47c97d5940c79c5c22bc04
daniel <darodriguezto@unal.edu.co>
2024-03-27 03:42:18 -0500

    Se añade la correlacióncruzaada del2013 y 2015 usando dos datos por semana, 104-106 datos por año, falta corregir calculo de incertidumbres y aplicar a demás años

--- correlation/Crosscorrelation.py
+++ correlation/Crosscorrelation.py
@@ -14,8 +14,7 @@ from statsmodels.graphics.tsaplots import plot_acf
 import argparse
 import os
 
-N = 104    
-aceptable = 1.96 / (N**0.5)
+
 
 # Utilizando argparse para ingresar el valor de "year"
 parser = argparse.ArgumentParser(description='Programa para análisis de series temporales.')
@@ -28,13 +27,16 @@ file = os.path.expanduser(file)  # Expande el path del usuario
 
 df = pd.read_csv(file)
 
+N = len(df.iloc[:,0])    
+aceptable = 1.96 / (N**0.5)
+
 Near = df.iloc[:, 1]
 Far = df.iloc[:, 2]
 sumastregnth=df.iloc[:,2].sum()
-prom=sumastregnth/104
+prom=sumastregnth/N
 
 # Convertir la columna de fechas al formato año-semana y establecerla como índice
-df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0] + '-1', format='%Y-%U-%w')  # Agregar '-1' para representar el día de la semana
+#df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0] + '-1', format='%Y-%U-%w')  # Agregar '-1' para representar el día de la semana
 df.set_index(df.columns[0], inplace=True)
 
 # Función para realizar la prueba de Dickey-Fuller
@@ -54,20 +56,20 @@ prueba_dickey_fuller(Near, 'Dependiente')
 
 # Graficar las series de tiempo con fechas como índice
 df.iloc[:, 0:].plot(subplots=True, figsize=(10, 6))
-plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/ambas.png'))
+plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/ambas(semanas_a_partidas).png'))
 plt.show()
 
 # Graficar la autocorrelación para ambas series
-plot_acf(Near, lags=13, title='Autocorrelación para el # de Nuevas AR')
-plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/autonear.png'))
+plot_acf(Near, lags=26, title='Autocorrelación para el # de Nuevas AR')
+plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/autonear(semanas_a_partidas).png'))
 plt.show()
 
-plot_acf(Far, lags=13, title='Autocorrelación para el Strength promedio')
-plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/autofar.png'))
+plot_acf(Far, lags=26, title='Autocorrelación para el Strength promedio')
+plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/autofar(semanas_a_partidas).png'))
 plt.show()
 
 # Supongamos que Near y Far son tus series temporales
-lag_max = 13  # Puedes ajustar este valor según tus necesidades
+lag_max = 26  # Puedes ajustar este valor según tus necesidades
 
 # Calcular la correlación cruzada en diferentes lags
 correlation_values = [np.corrcoef(Near, np.roll(Far, lag))[0, 1] for lag in range(-lag_max, lag_max+1)]
@@ -107,10 +109,11 @@ plt.stem(lags, correlation_values, basefmt='b-', use_line_collection=True)
 plt.title(f'Correlación cruzada entre Near y Far {args.year}')
 plt.xlabel('Lag')
 plt.ylabel('Correlación')
+plt.axhline(y=aceptable, color='r', linestyle='--', label='Aceptable')
 #plt.text(1.05, 0.95, f'D: {round(D, 4)}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top', bbox=dict(boxstyle='round', alpha=0.1))
 plt.text(1.05, 0.85, f'r: {round(r,4)}', transform=plt.gca().transAxes, fontsize=10, verticalalignment='top', bbox=dict(boxstyle='round', alpha=0.1))
 #plt.legend([D], loc='upper right')
 #plt.legend([r], loc='upper left')
 plt.subplots_adjust(right=0.8)
-plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/crosscorrelation_{args.year}.png'))
+plt.savefig(os.path.expanduser(f'~/Documentos/GoSA/Far_Side/FarSide-data/correlation/{args.year}/crosscorrelation_{args.year}(semanas_a_partidas).png'))
 plt.show()
